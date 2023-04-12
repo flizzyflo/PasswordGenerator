129a6f4d1d9f4805bbea538d663de15cc5952ce8
flizzyflo <vip3r1@gmx.de>
2023-04-12 08:20:01 +0200

    refactoring and renaming of variables

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -5,4 +5,4 @@ FONT_SETTINGS = ("ARIAL", 20, "bold")
 
 CHARACTER_LIST = [string.digits, string.ascii_lowercase, string.punctuation, string.ascii_uppercase]
 MIN_LENGTH = 8
-MAX_LENGTH =  sum(len(length) for length in CHARACTER_LIST)
+MAX_LENGTH = sum(len(length) for length in CHARACTER_LIST)
